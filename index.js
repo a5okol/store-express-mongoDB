73f693553ea330f9af4a9a49b838bf00fe33dbca
Andrew Sokolenko <a5okowork@gmail.com>
2020-04-05 22:13:01 +0300

    v.0.0.0.2 Handlebars. Динамический html

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
+
 const app = express();
 
-const PORT = process.env.PORT || 3000
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine) // региструем в express наличие движка handlebars
+app.set('view engine', 'hbs') // тут уже его используем
+app.set('views', 'views') // первый параметр - название переменной, второй - название папки где будут храниться все шаблоны
+
+app.get("/", (req, res) => {
+  // res.sendFile(path.join(__dirname, 'views', 'index.html'))
+  res.render('index')
+});
+
+app.get("/about", (req, res) => {
+    //res.sendFile(path.join(__dirname, 'views', 'about.html'))
+    res.render('about')
+  });
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Bro, Server is running on port ${PORT}`)
-})
+  console.log(`Bro, Server is running on port ${PORT}`);
+});

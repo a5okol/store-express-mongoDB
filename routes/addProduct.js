761da2a3b2132db807da5619dcfc3955a06cd64c
Andrew Sokolenko <a5okowork@gmail.com>
2020-04-06 16:25:49 +0300

    v.0.0.0.6 Создание формы добавление товаров

--- routes/addProduct.js
+++ routes/addProduct.js
@@ -2,10 +2,15 @@ const { Router } = require("express");
 const router = new Router();
 
 router.get("/", (req, res) => {
-    res.render('addProduct', {
-        title: 'Добавить товар',
-        isAddProduct: true
-      })
-})
+  res.render("addProduct", {
+    title: "Добавить товар",
+    isAddProduct: true,
+  });
+});
 
-module.exports = router
+router.post("/", (req, res) => {
+  console.log(req.body);
+  res.redirect('/add-product')
+});
+
+module.exports = router;

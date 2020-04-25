fd74cf4dc6275e74914cfd8ab23feb55aa0e3cde
Andrew Sokolenko <a5okowork@gmail.com>
2020-04-25 11:00:31 +0300

    v0.0.1.2 Устанавливаем Mongoose и Коннектим.

--- index.js
+++ index.js
@@ -2,40 +2,52 @@ const express = require("express");
 const path = require("path");
 const exphbs = require("express-handlebars");
 
-const homeRoutes = require('./routes/home')
-const tshirtsRoutes = require('./routes/tshirts')
-const sweatshirtsRoutes = require('./routes/sweatshirts')
-const hoodyRoutes = require('./routes/hoody')
-const shirtsRoutes = require('./routes/shirts')
-const backpackssRoutes = require('./routes/backpacks')
-const cardRoutes = require('./routes/card')
-const addProductRoutes = require('./routes/addProduct')
+const mongoose = require("mongoose");
+
+const homeRoutes = require("./routes/home");
+const tshirtsRoutes = require("./routes/tshirts");
+const sweatshirtsRoutes = require("./routes/sweatshirts");
+const hoodyRoutes = require("./routes/hoody");
+const shirtsRoutes = require("./routes/shirts");
+const backpackssRoutes = require("./routes/backpacks");
+const cardRoutes = require("./routes/card");
+const addProductRoutes = require("./routes/addProduct");
 
 const app = express();
 
 const hbs = exphbs.create({
-    defaultLayout: 'main',
-    extname: 'hbs'
-})
+  defaultLayout: "main",
+  extname: "hbs",
+});
 
-app.engine('hbs', hbs.engine) // региструем в express наличие движка handlebars
-app.set('view engine', 'hbs') // тут уже его используем
-app.set('views', 'views') // первый параметр - название переменной, второй - название папки где будут храниться все шаблоны
+app.engine("hbs", hbs.engine); // региструем в express наличие движка handlebars
+app.set("view engine", "hbs"); // тут уже его используем
+app.set("views", "views"); // первый параметр - название переменной, второй - название папки где будут храниться все шаблоны
 
-app.use(express.static(path.join(__dirname, 'public'))); // метод use позволяет добовлять middleware'ы
-app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, "public"))); // метод use позволяет добовлять middleware'ы
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/', homeRoutes)
-app.use('/t-shirts', tshirtsRoutes)
-app.use('/sweatshirts', sweatshirtsRoutes)
-app.use('/hoody', hoodyRoutes)
-app.use('/shirts', shirtsRoutes)
-app.use('/backpacks', backpackssRoutes)
-app.use('/card', cardRoutes)
-app.use('/add-product', addProductRoutes)
+app.use("/", homeRoutes);
+app.use("/t-shirts", tshirtsRoutes);
+app.use("/sweatshirts", sweatshirtsRoutes);
+app.use("/hoody", hoodyRoutes);
+app.use("/shirts", shirtsRoutes);
+app.use("/backpacks", backpackssRoutes);
+app.use("/card", cardRoutes);
+app.use("/add-product", addProductRoutes);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Bro, Server is running on port ${PORT}`);
-});
+async function start() {
+  try {
+    const url = `mongodb+srv://andrew:0uZrCRaZ5ORKmSXB@cluster0-tn1re.mongodb.net/test?retryWrites=true&w=majority`;
+    await mongoose.connect(url, { useNewUrlParser: true });
+    app.listen(PORT, () => {
+      console.log(`Bro, Server is running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+start();

cce2ce045473b8f92e6dc541bc525601acab6d3e
Andrew Sokolenko <a5okowork@gmail.com>
2020-05-05 22:24:19 +0300

    Добавление и сохранение сессии. Настройка сессии в БД

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const path = require("path");
 const exphbs = require("express-handlebars");
-
 const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoStore = require("connect-mongodb-session")(session);
+
+const Handlebars = require("handlebars");
+const {
+  allowInsecurePrototypeAccess,
+} = require("@handlebars/allow-prototype-access");
+
+const User = require("./models/user-model");
 
 const homeRoutes = require("./routes/home");
 const tshirtsRoutes = require("./routes/tshirts");
@@ -13,37 +21,36 @@ const backpackssRoutes = require("./routes/backpacks");
 const cardRoutes = require("./routes/card");
 const addProductRoutes = require("./routes/addProduct");
 const ordersRoutes = require("./routes/orders");
-const User = require("./models/user-model");
 const authRoutes = require("./routes/auth.js");
+const varMiddleware = require("./middleware/variables");
 
+const MONGODB_URI = `mongodb+srv://andrew:0uZrCRaZ5ORKmSXB@cluster0-tn1re.mongodb.net/shop`;
 const app = express();
-
-const Handlebars = require("handlebars");
-const {
-  allowInsecurePrototypeAccess,
-} = require("@handlebars/allow-prototype-access");
 const hbs = exphbs.create({
   defaultLayout: "main",
   extname: "hbs",
   handlebars: allowInsecurePrototypeAccess(Handlebars),
 });
+const store = new MongoStore({
+  collection: 'sessions',
+  uri: MONGODB_URI
+})
 
 app.engine("hbs", hbs.engine); // региструем в express наличие движка handlebars
 app.set("view engine", "hbs"); // тут уже его используем
 app.set("views", "views"); // первый параметр - название переменной, второй - название папки где будут храниться все шаблоны
 
-app.use(async (req, res, next) => {
-  try {
-    const user = await User.findById("5eaac15e6a2f74372baf0123");
-    req.user = user;
-    next();
-  } catch (err) {
-    console.log(err);
-  }
-});
-
 app.use(express.static(path.join(__dirname, "public"))); // метод use позволяет добовлять middleware'ы
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "some secret value",
+    resave: false,
+    saveUninitialized: false,
+    store
+  })
+);
+app.use(varMiddleware);
 
 app.use("/", homeRoutes);
 app.use("/t-shirts", tshirtsRoutes);
@@ -60,21 +67,12 @@ const PORT = process.env.PORT || 3000;
 
 async function start() {
   try {
-    const url = `mongodb+srv://andrew:0uZrCRaZ5ORKmSXB@cluster0-tn1re.mongodb.net/shop`;
-    await mongoose.connect(url, {
+    await mongoose.connect(MONGODB_URI, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
       useFindAndModify: false,
     });
-    const candidate = await User.findOne();
-    if (!candidate) {
-      const user = new User({
-        email: "andrew@gmail.com",
-        name: "Andrew",
-        cart: { items: [] },
-      });
-      await user.save();
-    }
+
     app.listen(PORT, () => {
       console.log(`Bro, Server is running on port ${PORT}`);
     });
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,29 @@
 const { Router } = require("express");
+const User = require("../models/user-model");
 const router = new Router();
 
 router.get("/login", async (req, res) => {
   res.render("auth/login", {
     title: "Авторизация",
     isLogin: true,
+  });
+});
+
+router.get("/logout", async (req, res) => {
+  req.session.destroy(() => {
+    res.redirect("/auth/login#login");
+  });
+});
+
+router.post("/login", async (req, res) => {
+  const user = await User.findById("5eaac15e6a2f74372baf0123");
+  req.session.user = user;
+  req.session.isAuthenticated = true;
+  req.session.save((err) => {
+    if (err) {
+      throw err;
+    }
+    res.redirect("/");
   });
 });
 

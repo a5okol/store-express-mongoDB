0b42b4c5c9b55ee2c9a7ba32512fcc8298535740
Andrew Sokolenko <a5okowork@gmail.com>
2020-05-13 22:59:06 +0300

    Настройка базовой валидации

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,8 @@ const bcryptjs = require("bcryptjs");
 const crypto = require("crypto");
 const nodemailer = require("nodemailer");
 const sendgrid = require("nodemailer-sendgrid-transport");
+const { body, validationResult } = require("express-validator/check");
+
 const User = require("../models/user-model");
 const keys = require("../keys");
 const regEmail = require("../emails/registration");
@@ -61,10 +63,17 @@ router.post("/login", async (req, res) => {
   }
 });
 
-router.post("/register", async (req, res) => {
+router.post("/register", body("email").isEmail(), async (req, res) => {
   try {
-    const { email, name, password, repeat } = req.body;
+    const { email, name, password, confirm } = req.body;
     const candidate = await User.findOne({ email });
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      req.flash("registerError", errors.array()[0].msg);
+      return res.status(422).redirect("/auth/login#register");
+    }
+    
     if (candidate) {
       req.flash("registerError", "Пользователь с таким email уже существует");
       res.redirect("/auth/login#register");
